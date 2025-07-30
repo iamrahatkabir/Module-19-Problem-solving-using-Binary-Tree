83713bac7d332afd7298107a8723c59db7652f58
Rahat Kabir Ifty Mac <iftyrahatkabir@gmail.com>
2025-07-30 06:01:05 +0600

    STL Pair

---
+++ 1_stl_pair.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <queue>
+#include <deque>
+#include <bitset>
+#include <iterator>
+#include <list>
+#include <stack>
+#include <map>
+#include <set>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <limits>
+#include <time.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+
+using namespace std;
+
+int main()
+{
+    pair<string, int> p;
+    p = {"Hello", 01};   // p = make_pair("hello", 90);
+
+
+    cout << p.first << endl;
+    cout << p.second << endl;
+
+    return 0;
+}
